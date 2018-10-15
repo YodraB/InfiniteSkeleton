3cc86fbbff81cc34101073c8e288f4bbcc2861d1
Yodra <YodraB@users.noreply.github.com>
2018-10-15 12:37:00 -0400

    Add basic movement

--- js/game.js
+++ js/game.js
@@ -54,7 +54,13 @@ window.focus();
 resizeGame();
 window.addEventListener("resize", resizeGame);
 
-// Bulk of the game code - init, preload, create, update
+// *Bulk of the game code - init, preload, create, update
+
+// Initiate scene params
+gameScene.init = function () {
+    // Player speed
+    this.playerSpeed = 50;
+};
 
 // Load assets
 gameScene.preload = function () {
@@ -66,12 +72,68 @@ gameScene.preload = function () {
 // Called once after preload
 gameScene.create = function () {
     // BG sprite
-    this.add.sprite(config.width / 2, config.height / 2, 'tiles');
+    this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'tiles');
     
-    // Player
-    // Get spawn location, create player sprite
+    // *Player
+    // Get spawn location
     var spawnX = gameValues.screenSize.col / 2,
-        spawnY = gameValues.screenSize.row / 2,
-        player = this.physics.add.sprite(gameValues.tileSize * (spawnX + 0.5), gameValues.tileSize * (spawnY + 0.5), 'player', 0);
-        //spawnX spawnY: randomInterval(1, gameValues.mapSize.col - 1); randomInterval(1, gameValues.mapSize.row - 1);
+        spawnY = gameValues.screenSize.row / 2; //spawnX spawnY: randomInterval(1, gameValues.mapSize.col - 1); randomInterval(1, gameValues.mapSize.row - 1);
+    
+    // Create player sprite
+    this.player = this.physics.add.sprite(gameValues.tileSize * (spawnX + 0.5), gameValues.tileSize * (spawnY + 0.5), 'player', 0);
+    
+    // Player animations
+    this.anims.create({
+      key: 'walk',
+      frames: this.anims.generateFrameNumbers('player', { start: 1, end: 2 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'stand',
+      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 0 }),
+      frameRate: 1
+    })
+    
+    // Keyboard input
+    this.cursors = this.input.keyboard.createCursorKeys();
+};
+
+// Called once per frame - default 60 fps
+gameScene.update = function () {
+    var prevVelocity = this.player.body.velocity.clone();
+    
+    // Player movement
+    this.player.body.setVelocity(0);
+
+    // Horizontal movement
+    if (this.cursors.left.isDown)
+    {
+        this.player.body.setVelocityX(-this.playerSpeed);
+        this.player.setFlipX(true);
+    }
+    else if (this.cursors.right.isDown || this.input.activePointer.isDown) //click/touch input here
+    {
+        this.player.body.setVelocityX(this.playerSpeed);
+        this.player.setFlipX(false)
+    }
+
+    // Vertical movement
+    if (this.cursors.up.isDown)
+    {
+        this.player.body.setVelocityY(-this.playerSpeed);
+    }
+    else if (this.cursors.down.isDown)
+    {
+        this.player.body.setVelocityY(this.playerSpeed);
+    }
+    
+    // Add animations
+    if (this.cursors.left.isDown || this.cursors.right.isDown || this.cursors.down.isDown || this.cursors.up.isDown || this.input.activePointer.isDown) {
+      this.player.anims.play("walk", true);
+    } else {
+      this.player.anims.play('stand');
+    }
+
 };
